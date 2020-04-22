093c360e5228d2ff7f189a1a3e4e3f06d7383001
Daniel Tavernese <45380503+DanielTavernese@users.noreply.github.com>
2020-04-22 12:41:00 -0400

    add lab 4

---
+++ labs/lab4/switches.h
@@ -0,0 +1,10 @@
+#ifndef _SWITCHES_H_
+#define _SWITCHES_H_
+
+
+/* define pointer macro for accessing the LED interface data register */
+
+#define SWITCHES	((volatile unsigned int *) 0x10000040)
+
+
+#endif /* _SWITCHES_H_ */
